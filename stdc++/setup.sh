09bd71d4cddf38916d09cd68c114e0fc11762c37
Roland Conybeare <roland@panix.com>
2022-04-25 13:54:56 -0400

    stdc++: + cleanup_phase

--- stdc++/setup.sh
+++ stdc++/setup.sh
@@ -185,6 +185,13 @@ function install_phase() {
     popd
 } # ..install_phase
 
+function cleanup_phase() {
+    pushd ${home}
+    rm -rf bin
+    rm -rf ${vsubdir}
+    popd
+} # ..cleanup_phase
+
 function do_all_phases() {
     display_phase
     unpack_phase
@@ -192,16 +199,20 @@ function do_all_phases() {
     configure_phase
     compile_phase
     install_phase
+    cleanup_phase
 } # ..do_all_phases
 
 function help() {
     echo "build sequence:"
+    echo "$ do_all_phases"
+    echo "or:"
     echo "$ display_phase"
     echo "$ unpack_phase"
     echo "$ patch_phase"
     echo "$ configure_phase"
     echo "$ compile_phase"
     echo "$ install_phase"
-}
+    echo "$ cleanup_phase"
+} # ..help
 
 # end setup.sh
